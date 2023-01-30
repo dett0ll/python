902c2799fca0aaffa14001320cca72d9aa855aeb
navjan <105518065+navjan@users.noreply.github.com>
2023-01-30 12:03:54 +0530

    Update and rename functions and variables to functions and variables.py

--- functions and variables.py
+++ functions and variables.py
@@ -30,3 +30,37 @@
 #print(round(x+y)) -  if we don’t need output as float but as integer
 #print(round(x+y), 2) -  if the float value is big and we need to round off to two decimal places
 #round(number[, ndigits]) — the one in square brackets is optional
+
+#if we want to use commas for the big numbers
+#a = 99999999999
+#b = 999999999999
+#c = a+b
+#print(f"{c:,}")
+#1,099,999,999,998
+
+#if we want float upto two decimal points
+#a = float(2.5335353)
+#b = float(2.456666)
+#c = a+b
+#print(f"{c:.2f}")
+#4.99
+
+#####################function###########################
+#def hello(to):
+#    print("hello", to)
+
+#name = input("enter name")
+#hello(name)
+#if we have a default parameter
+#def hello(to="abc"):
+#main() is the part of program which will run first. However we need to take care of scope. If we use return instead of print we will not get the answer because it will not print that
+
+def main():
+    name = input("what is your name")
+    hello(name)
+
+def hello(to):
+    print("hello",to )
+
+main() 
+
