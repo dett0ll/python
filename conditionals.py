91d8482479015d124ef91ea7190d0d2ab8eddef2
navjan <105518065+navjan@users.noreply.github.com>
2023-01-30 12:27:04 +0530

    Create conditionals.py

---
+++ conditionals.py
@@ -0,0 +1,11 @@
+#x = int (input("enter value of x"))
+#y = int (input("enter value of y"))
+
+#if x > y:
+#    print("x is greater that y")
+
+#elif x < y:
+#    print("x is less than y")
+
+#else   # we have not mentioned x=y as logically that is the only condition left
+#    print("x is equal to y")
