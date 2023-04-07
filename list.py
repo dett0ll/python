c91cb776325e1b909fdb0afd5a7cc41d5ade81d4
xssproto <105518065+xssproto@users.noreply.github.com>
2023-04-07 10:55:42 +0530

    Update list.py

--- list.py
+++ list.py
@@ -29,3 +29,40 @@ print(vegetables[::2]) # 1 step. It will start from 0 index and skip one element
 #['Potato', 'Cabbage', 'Onion', 'Carrot']
 #['Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot']
 #['Tomato', 'Cabbage', 'Carrot']
+
+Vegetables = ['Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot']
+
+print('Tomato' in Vegetables) #case sensitive
+Vegetables.append ('Peas') # appends item to the last of the list
+print(Vegetables)
+
+Vegetables.insert(3,'Raddish') # it will insert before the index 3
+print(Vegetables)
+
+Vegetables.remove('Raddish') #specify the item we need to remove
+print(Vegetables)
+
+Vegetables.pop() # it will remove the last item if index not specified
+print(Vegetables)
+
+Vegetables.pop(0) 
+print(Vegetables)
+
+# the difference between remove ans pop is that in remove we need to mention the item name while in pop we need to mention index
+
+del Vegetables[0] #delete item with 0 index
+print(Vegetables)
+
+del Vegetables
+print(Vegetables) # it shows undefined because the list vegtables is deleted
+#output
+#True
+#['Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot', 'Peas']
+#['Tomato', 'Potato', 'Cabbage', 'Raddish', 'Onion', 'Carrot', 'Peas']
+#['Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot', 'Peas']
+#['Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot']
+#['Potato', 'Cabbage', 'Onion', 'Carrot']
+#['Cabbage', 'Onion', 'Carrot']
+#Traceback (most recent call last):
+#  File "<string>", line 25, in <module>
+#NameError: name 'Vegetables' is not defined
