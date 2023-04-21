1b494c39a1a0c19812abc640a9a675ed1d60d28e
xssproto <105518065+xssproto@users.noreply.github.com>
2023-04-21 17:19:37 +0530

    Create 10_loops.py

---
+++ 10_loops.py
@@ -0,0 +1,34 @@
+#while
+#it will kepp on printing until the condition becomes false
+count = 0
+while count < 5:
+    print(count)
+    count = count + 1
+    
+#Break: We use break when we like to get out of or stop the loop
+count = 0
+while count < 5:
+    print(count)
+    count = count + 1
+    if (count==3):
+        break
+        
+#Continue: With the continue statement we can skip the current iteration, and continue with the next
+#it will skip 3
+count = 0
+while count < 5:
+    if count == 3:
+        continue
+    print(count)
+    count = count + 1
+    
+
+#for
+num = [1,2,3,4,5]
+for n in num:
+    print(n)
+
+name = 'John'
+for s in name:
+    print(s)
+    
