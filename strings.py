565e3cb7cfb323b43471bf1f12ef4e44129ca605
dett0l <105518065+dett0ll@users.noreply.github.com>
2024-08-01 17:16:43 +0530

    Update strings.py

--- strings.py
+++ strings.py
@@ -3,3 +3,50 @@ print(ord(a))
 #output is ASCII value = 97
 print(chr(97))
 #output will be a
+s = 'terminator'
+for i in range(len(s)):
+    print(s[i],end="")
+#output will print terminator
+s = 'terminator'
+for i in range(len(s)):
+    print("a",end="")
+#output will be "a" of the length terminator
+
+s = "terminator"
+print(s[1:3]) #start from s[1] till s[3] but do not include s[3]
+print(s[3:])#start from s[3] onwards including s[3]
+print(s[:3])#Do not include s[3] print values before s[3]
+print(s[3:-2])#start with s[3] and end at s[-2] which is second from reverse but do not include s[-2]
+print(s[-3:4]) # no output
+print(s[::2])#start with s[0] skip one place and move to s[2] then s[4]
+print(s[1::2])#start with s[1] and skip one place and move to s[3] then s[5]
+print(s[-2])#print second digit from reverse
+#er
+#minator
+#ter
+#minat
+
+#triao
+#emntr
+#o
+
+s = "terminator"
+print("e" in s) # True
+print("y" in s) # False
+print("y" not in s) # True
+
+#it does not support delete, append and insert
+
+s = "terminator"
+print(s+"s") # output is terminators
+
+s = "terminator"
+print(min(s)) # output will be a
+
+s = "terminatorA"
+print(min(s)) # output will be A because as per ASCII A is lower place than a
+
+s = "terminator"
+print(max(s)) # output will be t
+
+
