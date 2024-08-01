704894ef1732df61418124903ec643c281bdb90a
dett0l <105518065+dett0ll@users.noreply.github.com>
2024-08-01 12:58:59 +0530

    Create platform.py

---
+++ platform.py
@@ -0,0 +1,16 @@
+from platform import platform, machine, processor, system, version, python_implementation
+ 
+print(platform())
+print(machine())
+print(processor())
+print(system())
+print(version())
+print(python_implementation())
+
+#output
+#Windows-10-10.0.19045-SP0
+#AMD64
+#Intel64 Family 6 Model 42 Stepping 7, GenuineIntel
+#Windows
+#10.0.19045
+#CPython
